892df882e76579769e7b903ce9b23c87e206ea2a
fweber13 <fweber13@smail.uni-koeln.de>
2020-02-15 10:55:14 +0100

    General: initialization of components

---
+++ src/app/gymbuddies/gymbuddies.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gymbuddies',
+  templateUrl: './gymbuddies.component.html',
+  styleUrls: ['./gymbuddies.component.scss']
+})
+export class GymbuddiesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

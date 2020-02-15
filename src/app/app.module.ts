c70a79e5b4d53a2fbc492ca3b268278433f68bb7
fweber13 <fweber13@smail.uni-koeln.de>
2020-02-15 11:23:30 +0100

    General: remove header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { HeaderComponent } from './header/header.component';
 import { GymbuddiesComponent } from './gymbuddies/gymbuddies.component';
 import { PlansComponent } from './plans/plans.component';
 import { MerchandiseComponent } from './merchandise/merchandise.component';
-import { MenuComponent } from './header/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent,
     GymbuddiesComponent,
     PlansComponent,
     MerchandiseComponent,
-    MenuComponent
   ],
   imports: [
     BrowserModule,

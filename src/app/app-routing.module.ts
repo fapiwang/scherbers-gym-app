284aed9a65a13baaf1e33037bf47e5b5c21bc3e3
fweber13 <fweber13@smail.uni-koeln.de>
2020-02-15 14:09:58 +0100

    General: implement routes, navbar, menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { GymbuddiesComponent } from './gymbuddies/gymbuddies.component';
+import { PlansComponent } from './plans/plans.component';
+import { MerchandiseComponent } from './merchandise/merchandise.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'gymbuddies', component: GymbuddiesComponent },
+  { path: 'plans', component: PlansComponent },
+  { path: 'merchandise', component: MerchandiseComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

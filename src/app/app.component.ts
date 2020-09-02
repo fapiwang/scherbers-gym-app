24df9a5b8fc4e1b7f76a21a9e4196fc8ee2fc970
fweber13 <fweber13@smail.uni-koeln.de>
2020-09-02 17:01:38 +0200

    Auth: add amplify auth and restructure app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectorRef } from '@angular/core';
+import { Component, ChangeDetectorRef, OnInit, OnDestroy, ApplicationRef, Testability } from '@angular/core';
 import { onAuthUIStateChange, CognitoUserInterface, AuthState } from '@aws-amplify/ui-components';
 
 @Component({
@@ -6,12 +6,21 @@ import { onAuthUIStateChange, CognitoUserInterface, AuthState } from '@aws-ampli
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'scherbers-gym-app';
   user: CognitoUserInterface | undefined;
   authState: AuthState;
 
   isMenuOpen = false;
+
+  toggleMenu() {
+    if (this.authState !== 'signedin') {
+      this.isMenuOpen = false;
+    }
+    if (this.authState === 'signedin') {
+      this.isMenuOpen = !this.isMenuOpen;
+    }
+  }
 
   constructor(private ref: ChangeDetectorRef) { }
 
@@ -20,14 +29,11 @@ export class AppComponent {
       this.authState = authState;
       this.user = authData as CognitoUserInterface;
       this.ref.detectChanges();
-    })
+    });
   }
 
   ngOnDestroy() {
     return onAuthUIStateChange;
   }
 
-  toggleMenu() {
-    this.isMenuOpen = !this.isMenuOpen;
-  }
 }
